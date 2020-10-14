c305d684990bcf221822edb84655b280f3ee5319
Lena <himym1989@gmail.com>
2020-10-14 08:01:03 +0300

    refactory

--- src/com/shpp/p2p/cs/lzhukova/assignment3/Assignment3Part3.java
+++ src/com/shpp/p2p/cs/lzhukova/assignment3/Assignment3Part3.java
@@ -4,29 +4,30 @@ import com.shpp.cs.a.console.TextProgram;
 
 public class Assignment3Part3 extends TextProgram {
 
+    double base;
+    int exponent;
+
     public void run() {
-        println(raiseToPower((readDouble("put a number")), readInt("put an exponent")));
+        base = readDouble("put a number");
+        exponent = readInt("put an exponent");
+
+        println(raiseToPower(base, exponent));
     }
 
-    private double raiseToPower(double base, int exponent) {
-        double multiplierNum = base;
-        double result = base;
+    private double raiseToPower(double x, int y) {
+        x = (exponent < 0) ? (1 / base) : base;
+        y = exponent < 0 ? -exponent : exponent;
+        double result = x;
 
         if (exponent == 0) {
             return 1;
         }
-        for (int i = 1; i < exponent; i++) {
-            if (exponent > 0) {
-                //getRegularFraction();
-                result = result * multiplierNum;
-            }
+
+        for (int i = 1; i < y; i++) {
+            //getRegularFraction();
+            result = result * x;
         }
         return result;
     }
 
-    /*private double getRegularFraction (int numerator, int denominator) {
-
-    }
-
-     */
 }
