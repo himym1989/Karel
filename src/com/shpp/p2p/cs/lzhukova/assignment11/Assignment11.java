8801744e4d173d8b3b0024d4fcb5b8cb49074f95
Lena <himym1989@gmail.com>
2021-03-24 11:39:53 +0200

    Refactor calculator ass11

--- src/com/shpp/p2p/cs/lzhukova/assignment11/Assignment11.java
+++ src/com/shpp/p2p/cs/lzhukova/assignment11/Assignment11.java
@@ -1,75 +1,60 @@
 package com.shpp.p2p.cs.lzhukova.assignment11;
 
+
 import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 /**
- * This program is a calculator, that implements evaluation of numeric expressions using shunting-yard algorithm.
- * It supports such operators as: "-", "+", "/", "*" and "^"
- * and following math functions: sin, cos, tan, atan, log10, log2, sqrt.
- * It handles negative numbers as well as fractions.
+ * This program implements calculator, that supports the following operators and functions:
+ * "-", "+", "/", "*" , "^";
+ * sin, cos, tan, atan, log10, log2, sqrt;
  */
 
 public class Assignment11 {
 
-    /* variables, that will be parsed from the command line*/
-    private static HashMap<String, Double> variables = new HashMap();
+    /* hashmap, that will contain variables with their values */
+    private static HashMap<String, Double> vars = new HashMap<>();
 
-    public static void main(String[] args) {
 
-        Calculator calc = new Calculator();
+    public static void main(String[] args) throws Exception {
+        checkArguments(args);
 
-        try {
-            String formula = args[0];
-            variables = extractVariables(args);
-            calc.parenthesisCheck(formula);
-            if (!calc.parenthesisCheck(formula)) {
-                System.out.println("One of the parenthesis in your expression is missed. Check and try again.");
-            } else {
-                System.out.println(
-                        "The result of the numerical expression "
-                                + formula + (variables.isEmpty() ? "" : ", where " + variables.entrySet())
-                                + " is " + calc.calculate(formula, variables) + ".");
-            }
-        } catch (Exception e) {
-            if (args.length == 0) {
-                System.out.println("You haven't input any expression");
-            } else {
-                System.out.println("Something went wrong. Check your expression and try again");
-            }
+        Calculator calculator = new Calculator();
+        String mathExpression = args[0].toLowerCase().replaceAll(" ", "");
+        vars = extractVars(args);
+
+        System.out.println("The result of math expression " + mathExpression +
+                (vars.isEmpty() ? "" : ", where variables are " + vars + " is...." +
+                        calculator.calculate(mathExpression, vars)));
+    }
+
+    private static void checkArguments(String[] args) throws Exception {
+        if (args.length == 0) {
+            throw new Exception("You haven't input any expression");
         }
     }
 
 
     /**
-     * This method implements parsing variables from the command line and
-     * saving them to the hashmap, where key is a variable and value is a number value of the variable.
+     * This method implements extracting variables and their values from program arguments
+     * and putting them to the hashmap;
+     *
+     * @param args - String array from the command line;
+     * @return vars, Hashmap with vars and values;
      */
-    private static HashMap<String, Double> extractVariables(String[] args) throws Exception {
-        /* save the value before putting it to the hashmap */
-        String buffer;
-        String key = "";
-        double value = 0;
-        Pattern p = Pattern.compile("((\\-?\\d*\\.?\\d+)|[a-zA-Z])");
-
+    private static HashMap<String, Double> extractVars(String[] args) throws Exception {
         for (int i = 1; i < args.length; i++) {
+            args[i] = args[i].replaceAll(" ", "");
+            String[] buffer = args[i].split("=");
 
-            Matcher m = p.matcher(args[i]);
-
-            while (m.find()) {
-                buffer = m.group();
-                if (buffer.matches("([a-zA-Z])")) {
-                    key = buffer;
-                } else if (buffer.matches("(\\-?\\d*\\.?\\d+)")) {
-                    value = Double.parseDouble(buffer);
-                }
+            if (buffer.length <= 1) {
+                throw new Exception("Check your variables. Maybe you haven't input some variable's value.");
+            } else {
+                String var = buffer[0].toLowerCase();
+                double value = Double.parseDouble(buffer[1]);
+                vars.put(var, value);
             }
-            variables.put(key, value);
         }
-        return variables;
+        return vars;
     }
 }
-
-
--- src/com/shpp/p2p/cs/lzhukova/assignment11/Calculator.java
+++ src/com/shpp/p2p/cs/lzhukova/assignment11/Calculator.java
@@ -2,108 +2,92 @@ package com.shpp.p2p.cs.lzhukova.assignment11;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class Calculator {
-    // stack will keep math signs of the expression
-    private final Stack<String> operators = new Stack<>();
-
-    // arraylist will keep numbers and signs as well
-    private final ArrayList<String> numAndOperators = new ArrayList<>();
+public class Calculator {
 
     MathOperations operations = new MathOperations();
     MathFunctions functions = new MathFunctions();
 
-    /**
-     * This method implements evaluation of an numeric expression,
-     * transforming infix to postfix expression at first.
-     */
-    public double calculate(String arg, HashMap<String, Double> variables) throws Exception {
-        sortOperatorsAndNums(arg, variables);
-        return run();
+    private ArrayList<String> expression = new ArrayList<>();
+
+
+    public Double calculate(String mathExpression, HashMap<String, Double> vars) throws Exception {
+        expression = mathExpressionAnalyze(mathExpression, vars);
+        return doMath(expression, vars);
     }
 
-    public boolean parenthesisCheck(String arg) {
-        int leftParenthesis = 0;
-        int rightParenthesis = 0;
-        for (int i = 0; i < arg.length(); i++) {
-            if (arg.charAt(i) == '(') {
-                leftParenthesis += 1;
-            }
-            if (arg.charAt(i) == ')') {
-                rightParenthesis += 1;
+    private double doMath(ArrayList<String> expression, HashMap<String, Double> vars) {
+        while (expression.size() != 1) {
+            for (int i = 0; i < expression.size(); i++) {
+                String element = expression.get(i);
+                if (isOperator(element)) {
+                }
             }
         }
-        return leftParenthesis == rightParenthesis;
+
+
+        return 0.0;
     }
 
+    private ArrayList<String> mathExpressionAnalyze(String mathExpression, HashMap<String, Double> vars) throws Exception {
 
-    /**
-     * This method implements first stage of shunting-yard algorithm:
-     * sorting of numbers and math signs to the stack or to the arraylist.
-     * Numbers are added to arraylist in any case. For signs there are some rules:
-     * - if stack is empty -  the first signs is pushed to stack in any case;
-     * - if the priority of the previous sign in the stack is higher or equal to the priority of the
-     * current sign, that we check(look for hashmap "operatorPriority"),
-     * the previous sign is popped from the stack and added to arraylist,
-     * and the current sign is pushed to the stack.
-     *
-     * @param arg       the math expression.
-     * @param variables hashmap, that keeps variables with their values.
-     */
-    private void sortOperatorsAndNums(String arg, HashMap<String, Double> variables) throws Exception {
-        Pattern pattern = Pattern.compile(functions.buildRegexp() + "|" + "((\\d*\\.?\\d+)|[a-zA-Z]|" + operations.buildRegexp() + ")");
-        Matcher matcher = pattern.matcher(arg);
+        /* this classes are used for finding matches in the math expression with the following patterns - regexp.
+         * Nothing redundant will be saved to the arraylist with the expression - just numbers, variables,
+         * operations and functions */
+        Pattern pattern = Pattern.compile(functions.buildRegexp() + "|" + "((\\d*\\.?\\d+)|([a-z]+)|" + operations.buildRegexp() + ")");
+        Matcher matcher = pattern.matcher(mathExpression);
 
-        // check if the first number is negative;
-        if (arg.charAt(0) == '-') {
-            numAndOperators.add("0");
+        if (mathExpression.charAt(0) == '-') {
+            expression.add("0");
         }
 
-        // buffer String, tha will keep the value of previous match.
-        String previousVal = null;
-        // boolean variable, that helps mark if the next number is negative or not
         boolean isNegative = false;
+        String previousValue = "";
+
 
         while (matcher.find()) {
-            String val = matcher.group();
+            String value = matcher.group();
 
-            // if there are two operators in a row, and the second is "-"
-            // it means, that the next number will be negative;
-            if (val.equals("-") && (isOperator(previousVal) && !previousVal.equals(")"))) {
+            if (value.equals("-") && (isOperator(previousValue))) {
                 isNegative = true;
                 continue;
             }
-            if (isGroupOperator(val) || isOperator(val) || isFunction(val)) {
-                addOperator(val);
-            } else if (val.matches("[a-zA-Z]")) {
-                if (variables.get(val) == null) {
-                    throw new Exception("variable `" + val + " has no value");
-                }
-                numAndOperators.add(variables.get(val).toString());
-            } else {
-                if (isNegative) {
-                    numAndOperators.add("-" + val);
-                    isNegative = false;
+            if (isValue(value)) {
+                if (!isVarPresent(value, vars)) {
+                    throw new ArithmeticException("There is no value for variable: \"" + value + "\"");
                 } else {
-                    numAndOperators.add(val);
+                    expression.add(isNegative ? "-" + value : value);
+                    isNegative = !isNegative;
                 }
+            } else if (isNegative) {
+                expression.add("-" + value);
+                isNegative = false;
+            } else {
+                expression.add(value);
             }
-            previousVal = val;
-            System.out.println(operators);
+            previousValue = value;
         }
 
-        while (!operators.empty()) {
-            numAndOperators.add(operators.pop());
-        }
+        System.out.println(expression.toString());
 
-        // remove remaining parentheses from the arraylist
-        numAndOperators.removeIf(o -> o.equals("(") || o.equals(")"));
-        System.out.println(numAndOperators);
-
+        return expression;
     }
+
+    private boolean isValue(String value) {
+        return value.matches("[a-z]+") && !isFunction(value);
+    }
+
+
+    /**
+     * This method implements check if the variable in the math expression attends in the hashmap
+     * in order to get its value later;
+     */
+    private boolean isVarPresent(String value, HashMap<String, Double> vars) {
+        return vars.get(value) != null;
+    }
+
 
     /**
      * This method checks if the value is an operator or not.
@@ -126,116 +110,4 @@ class Calculator {
         return val != null && val.matches(functions.buildRegexp());
     }
 
-    /**
-     * Method, that starts the calculation directly.
-     *
-     * @return double, result of evaluation.
-     */
-    private double run() {
-        Stack<String> expression = new Stack<>();
-        double result = 0;
-
-        while (numAndOperators.size() != 0) {
-            String removedItem = numAndOperators.remove(0);
-
-            if (removedItem.matches("(\\-?\\d*\\.?\\d+)")) {
-                expression.push(removedItem);
-            } else if (removedItem.matches(functions.buildRegexp())) {
-                double operand = Double.parseDouble(expression.pop());
-
-                result = math(removedItem, operand);
-                expression.push(String.valueOf(result));
-
-            } else {
-                double secondOperand = Double.parseDouble(expression.pop());
-                double firstOperand = Double.parseDouble(expression.pop());
-
-                result = math(removedItem, firstOperand, secondOperand);
-                expression.push(String.valueOf(result));
-            }
-        }
-        return result;
-    }
-
-    /**
-     * This method implements:
-     * - check of signs priority;
-     * - signs' moves from stack to arraylist, if it is needed.
-     */
-    private void addOperator(String operator) {
-        MathOperation operation = operations.map.get(operator);
-        MathFunction function = functions.funcMap.get(operator);
-
-        int operatorPriority;
-        // priority of the previous operator, that is currently at the top of the stack
-        int prevOperatorPriority;
-
-        // check for parenthesis in an expression and adding all the operators between them
-        // to the arraylist
-        if (isGroupOperator(operator)) {
-            if (!operators.isEmpty() && operator.equals(")")) {
-                String prevOperator;
-                do {
-                    prevOperator = operators.pop();
-                    numAndOperators.add(prevOperator);
-                }
-                while (!operators.isEmpty() && !prevOperator.equals("("));
-                numAndOperators.add(operator);
-            } else {
-                operators.push(operator);
-
-            }
-            return;
-        }
-
-        // check if "operator" is an operator or a function for getting its priority
-        if (isOperator(operator)) {
-            operatorPriority = operation.getPriority();
-        } else {
-            operatorPriority = function.getPriority();
-        }
-
-        if (!operators.empty()) {
-            do {
-                String prevOperator = operators.pop();
-
-                MathOperation prevOperation = operations.map.get(prevOperator);
-                MathFunction preVFunction = functions.funcMap.get(prevOperator);
-
-                if (isFunction(prevOperator)) {
-                    prevOperatorPriority = preVFunction.getPriority();
-                } else {
-                    prevOperatorPriority = prevOperation.getPriority();
-                }
-
-                if (prevOperatorPriority >= operatorPriority) {
-                    numAndOperators.add(prevOperator);
-                } else {
-                    operators.push(prevOperator);
-                }
-            }
-            while (prevOperatorPriority > operatorPriority && !operators.empty()) ;
-        }
-
-        operators.push(operator);
-    }
-
-    /**
-     * Method implements doing math depending on an operator or function;
-     *
-     * @return double, result of math operation;
-     */
-    private double math(String operator, double x, double y) {
-        if (!operations.map.containsKey(operator)) {
-            throw new IllegalStateException("Unexpected operator: " + operator);
-        }
-        return operations.map.get(operator).calc(x, y);
-    }
-
-    private double math(String operator, double x) {
-        if (!functions.funcMap.containsKey(operator)) {
-            throw new IllegalStateException("Unexpected operator: " + operator);
-        }
-        return functions.funcMap.get(operator).calc(x);
-    }
 }
