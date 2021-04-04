907375a96bbbe820a60f6f86dd5bc50d8ad38cd8
Lena <himym1989@gmail.com>
2021-04-04 12:46:00 +0300

    Deep-first search ass 12

--- src/com/shpp/p2p/cs/lzhukova/assignment12/FindingSilhouettes.java
+++ src/com/shpp/p2p/cs/lzhukova/assignment12/FindingSilhouettes.java
@@ -2,50 +2,68 @@ package com.shpp.p2p.cs.lzhukova.assignment12;
 
 import acm.graphics.GImage;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class FindingSilhouettes {
+    private final ArrayList<Integer> Silhouettes;
     int SilhouettesCount;
     int[][] pixelArr;
     HashMap<Integer, Boolean> vertices;
-    private int vertexNum = 0;
-    private int firstVertex;
-
-    {
-        SilhouettesCount += 1;
-    }
-
-    {
-        SilhouettesCount += 1;
-    }
+    private int vertexNum;
+    private int counter = 0;
+    private int pixelsNum = 0;
 
     public FindingSilhouettes(GImage image) {
+        Silhouettes = new ArrayList<>();
+
         SilhouettesCount = 0;
+
         vertices = new HashMap<>();
         pixelArr = image.getPixelArray();
         getVertices();
         find();
     }
 
-    public int find() {
-        while(true) {
-            dfs(firstVertex);
-            SilhouettesCount+=1;
-            System.out.println("Silhouettes " + SilhouettesCount);
-            lookForSilhouettes();
-            return 0;
+    public void find() {
+        lookForSilhouette();
+        countCorrectSilhouettes();
+    }
+
+    private void countCorrectSilhouettes() {
+        for (int i = 0; i < Silhouettes.size(); i++) {
+            if (Silhouettes.get(i) > 150)
+                SilhouettesCount += 1;
+        }
+        System.out.println((SilhouettesCount <= 1 ? "There is " : "There are ") + SilhouettesCount +
+                (SilhouettesCount == 1 ? " silhouette " : " silhouettes ") +
+                "at the picture.");
+    }
+
+    private void lookForSilhouette() {
+
+        for (Map.Entry<Integer, Boolean> entry : vertices.entrySet()) {
+            if (Objects.equals(false, entry.getValue())) {
+                int startVertex = entry.getKey();
+
+                Silhouettes.add(counter, pixelsNum);
+
+                dfs(startVertex);
+                pixelsNum = 0;
+                counter += 1;
+            }
 
         }
     }
 
-    private void lookForSilhouettes() {
-
-    }
-
 
     private void dfs(int vertex) {
+        pixelsNum += 1;
+        Silhouettes.set(counter, pixelsNum);
         vertices.put(vertex, true);
-        System.out.println(vertex);
+//        System.out.println(vertex);
 
         int down = vertex + pixelArr[0].length;
         int up = vertex - pixelArr[0].length;
@@ -58,11 +76,11 @@ public class FindingSilhouettes {
         if (vertices.containsKey(right) && !vertices.get(right)) {
             dfs(right);
         }
-        if (vertices.containsKey(left) && !vertices.get(left)) {
-            dfs(left);
-        }
         if (vertices.containsKey(up) && !vertices.get(up)) {
             dfs(up);
+        }
+        if (vertices.containsKey(left) && !vertices.get(left)) {
+            dfs(left);
         }
     }
 
@@ -71,24 +89,43 @@ public class FindingSilhouettes {
         int bgRed = GImage.getRed(pixelArr[y][x]);
         int bgGreen = GImage.getGreen(pixelArr[y][x]);
         int bgBlue = GImage.getBlue(pixelArr[y][x]);
-        return GImage.createRGBPixel(bgRed, bgGreen, bgBlue);
+        return (bgRed + bgGreen + bgBlue) / 3;
     }
 
     private void getVertices() {
-        int BGColor = getPixelColor(0, 0);
+        int BGAlpha = getAlpha(0, 0);
+        System.out.println(BGAlpha);
+        if (BGAlpha != 0) {
+            searchVerticesForColor();
+        } else {
+            searchVerticesForAlpha(BGAlpha);
+        }
+
+    }
+
+    private void searchVerticesForAlpha(int BGAlpha) {
         for (int y = 0; y < pixelArr.length; y++) {
             for (int x = 0; x < pixelArr[0].length; x++) {
                 vertexNum += 1;
-
-                if (getPixelColor(y, x) != BGColor) {
+                if (getAlpha(y, x) != BGAlpha) {
                     vertices.put(vertexNum, false);
-                    if (firstVertex == 0) {
-                        firstVertex = vertexNum;
-                    }
                 }
             }
         }
     }
 
+    private void searchVerticesForColor() {
+        for (int y = 0; y < pixelArr.length; y++) {
+            for (int x = 0; x < pixelArr[0].length; x++) {
+                vertexNum += 1;
+                if (getPixelColor(y, x) < 150) {
+                    vertices.put(vertexNum, false);
+                }
+            }
+        }
+    }
 
+    private int getAlpha(int y, int x) {
+        return GImage.getAlpha(pixelArr[y][x]);
+    }
 }
