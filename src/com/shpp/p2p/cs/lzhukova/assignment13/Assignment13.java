2d2a141208139db826f8ffcf6993098b436ee358
Lena <himym1989@gmail.com>
2021-04-08 11:01:04 +0300

    Deep-first search ass 12

---
+++ src/com/shpp/p2p/cs/lzhukova/assignment13/Assignment13.java
@@ -0,0 +1,53 @@
+package com.shpp.p2p.cs.lzhukova.assignment13;
+
+import acm.graphics.GImage;
+import com.shpp.p2p.cs.lzhukova.assignment13.FindSilhouettes;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class Assignment13 {
+
+
+    private static final String TEST_PICTURE = "src/com/shpp/p2p/cs/lzhukova/assignment13/assets/test3.jpg";
+
+    private static final String[] FILTERS = {".png", ".jpg", ".jpeg"};
+
+
+    public static void main(String[] args) {
+        GImage image;
+        String filePath;
+
+
+        if (args != null && args.length > 0 && !args[0].equals("")) {
+            filePath = Paths.get(args[0]).toAbsolutePath().toString();
+        } else {
+            filePath = Paths.get(TEST_PICTURE).toAbsolutePath().toString();
+        }
+        if (acceptFileType(new File(filePath))) {
+            try {
+                image = new GImage(filePath);
+                FindSilhouettes f = new FindSilhouettes(image);
+                System.out.println(f.printResult());
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println("Something goes wrong with your file or path to it. Check and try again!");
+            }
+
+        } else {
+            System.err.println("Incorrect file type!");
+            System.exit(-1);
+        }
+
+    }
+
+    public static boolean acceptFileType(File filename) {
+        return filename.isDirectory() || Arrays.asList(FILTERS).contains(extensionOf(filename));
+    }
+
+    private static String extensionOf(File filename) {
+        int lastDot = filename.getName().lastIndexOf(46);
+        return lastDot == -1 ? "" : filename.getName().substring(lastDot);
+    }
+}
---
+++ src/com/shpp/p2p/cs/lzhukova/assignment13/FindSilhouettes.java
@@ -0,0 +1,179 @@
+package com.shpp.p2p.cs.lzhukova.assignment13;
+
+import acm.graphics.GImage;
+
+import java.util.*;
+
+public class FindSilhouettes implements SilhouettesParamConstants {
+
+    private final ArrayList<Integer> Silhouettes;
+
+    int SilhouettesCounter;
+
+    /**
+     * 2d - array, that is got from the picture;
+     */
+    int[][] pixelArr;
+
+    /**
+     * Hashmap, that contains number of vertex and its status:
+     * - false, if vertex is not visited;
+     * - true, if vertex is visited;
+     */
+    HashMap<Integer, Boolean> vertices;
+
+    private int vertexCounter;
+    private int pixelsCounter;
+
+    /**
+     * counter-variable, that contains amount of pixels in each silhouette;
+     */
+    private int pixelsAmount;
+
+    public FindSilhouettes(GImage image) {
+        Silhouettes = new ArrayList<>();
+        SilhouettesCounter = 0;
+        vertices = new HashMap<>();
+
+        pixelArr = image.getPixelArray();
+        searchVertices();
+        lookForSilhouettes();
+    }
+
+    private void countSilhouettes() {
+        for (Integer silhouette : Silhouettes) {
+            if (silhouette > (pixelArr.length * pixelArr[0].length) * MIN_SCALE) {
+                SilhouettesCounter += 1;
+            }
+        }
+    }
+
+    /**
+     * Method implements search of available silhouettes at the picture,
+     * and  namely - search in the hashmap for "false" values,
+     * that means, that such vertices weren't visited before;
+     */
+    public void lookForSilhouettes() {
+
+        for (Map.Entry<Integer, Boolean> entry : vertices.entrySet()) {
+            if (Objects.equals(false, entry.getValue())) {
+                int startVertex = entry.getKey();
+
+                Silhouettes.add(pixelsCounter, pixelsAmount);
+
+                bfs(startVertex);
+
+                pixelsCounter += 1;
+                pixelsAmount = 0;
+            }
+        }
+        countSilhouettes();
+        printResult();
+    }
+
+    private void bfs(int vertex) {
+        LinkedList<Integer> listOfVertices = new LinkedList<Integer>();
+        listOfVertices.add(vertex);
+
+
+        while (!listOfVertices.isEmpty()) {
+
+            pixelsAmount += 1;
+            Silhouettes.set(pixelsCounter, pixelsAmount);
+
+            int visitedVertex = listOfVertices.removeFirst();
+
+            vertices.put(visitedVertex, true);
+
+            int down = visitedVertex + pixelArr[0].length;
+            int up = visitedVertex - pixelArr[0].length;
+            int right = visitedVertex + 1;
+            int left = visitedVertex - 1;
+
+
+            int[] directions = {down, up, right, left};
+            for (int direction : directions) {
+                if (vertices.containsKey(direction) && !vertices.get(direction) && !listOfVertices.contains(direction)) {
+                    listOfVertices.add(direction);
+                }
+            }
+
+        }
+
+    }
+
+    public String printResult() {
+        return (SilhouettesCounter <= 1 ? "There is " : "There are ") + SilhouettesCounter +
+                (SilhouettesCounter <= 1 ? " silhouette " : " silhouettes ") +
+                "at the picture.";
+    }
+
+
+    /**
+     * Method implements deep-first-search. Amount of pixels in each silhouettes is saved to the
+     * arrayList for later verification;
+     *
+     * @param vertex
+     */
+    private void dfs(int vertex) {
+        pixelsAmount += 1;
+        Silhouettes.set(pixelsCounter, pixelsAmount);
+
+        vertices.put(vertex, true);
+
+        int down = vertex + pixelArr[0].length;
+        int up = vertex - pixelArr[0].length;
+        int right = vertex + 1;
+        int left = vertex - 1;
+
+        if (vertices.containsKey(down) && !vertices.get(down)) {
+            dfs(down);
+        }
+
+        if (vertices.containsKey(right) && !vertices.get(right)) {
+            dfs(right);
+        }
+        if (vertices.containsKey(up) && !vertices.get(up)) {
+            dfs(up);
+        }
+        if (vertices.containsKey(left) && !vertices.get(left)) {
+            dfs(left);
+        }
+    }
+
+    /**
+     * Method implements getting luminance of a particular pixel;
+     */
+    private double getPixelLuminance(int pixel) {
+        int red = GImage.getRed(pixel);
+        int green = GImage.getGreen(pixel);
+        int blue = GImage.getBlue(pixel);
+        return (red * 0.2126f + green * 0.7152f + blue * 0.0722f) / 255;
+    }
+
+    /**
+     * This method implements search of vertices in the 2d-array;
+     * Pixels with less then minimum valuable alpha and minimum valuable luminance will be saved
+     * to the hashmap for later silhouettes search;
+     */
+    private void searchVertices() {
+        boolean isLightBg = GImage.getAlpha(pixelArr[0][0]) == 0 || getPixelLuminance(pixelArr[0][0]) > MIN_VALUABLE_LUMINANCE;
+
+        for (int y = 0; y < pixelArr.length; y++) {
+            for (int x = 0; x < pixelArr[0].length; x++) {
+                vertexCounter += 1;
+
+                int alpha = GImage.getAlpha(pixelArr[y][x]);
+                if (alpha < MIN_VALUABLE_ALPHA) {
+                    continue;
+                }
+
+                boolean isLightPixel = getPixelLuminance(pixelArr[y][x]) > MIN_VALUABLE_LUMINANCE;
+
+                if ((isLightBg && !isLightPixel) || (!isLightBg && isLightPixel)) {
+                    vertices.put(vertexCounter, false);
+                }
+            }
+        }
+    }
+}
