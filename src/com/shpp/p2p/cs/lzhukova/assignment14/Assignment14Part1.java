5c38184b351d9083b5bb0a73a5f42e9c5d55476e
Lena <himym1989@gmail.com>
2021-04-19 20:20:01 +0300

    ass 14 refactoring

--- src/com/shpp/p2p/cs/lzhukova/assignment14/Assignment14Part1.java
+++ src/com/shpp/p2p/cs/lzhukova/assignment14/Assignment14Part1.java
@@ -1,39 +1,96 @@
 package com.shpp.p2p.cs.lzhukova.assignment14;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Assignment14Part1 {
-
-    private static final String IN_FILE = "src/com/shpp/p2p/cs/lzhukova/assignment14/assets/d.txt";
-    private static final String OUT_FILE = "src/com/shpp/p2p/cs/lzhukova/assignment14/assets/d.txt.par";
+    private static final String pathToFile = "src/com/shpp/p2p/cs/lzhukova/assignment14/assets/";
+    private static final String ARCHIVE_EXTENSION = ".par";
+    private static final String UNARCHIVE_EXTENSION = ".uar";
+    private static final String DEFAULT_IN_FILE = "test.txt";
+    private static final String DEFAULT_OUT_FILE = "test.txt.par";
+    private static final String FLAG_ARCHIVE = "-a";
+    private static final String FLAG_UNARCHIVE = "-u";
 
     public static void main(String[] args) {
-        Compressor compressor = new Compressor();
+        String forArchiveFile = "";
+        String archivedFile = "";
+        String uarFile = "";
+
+        boolean toArchive = false;
+
+        if (args.length == 0) {
+            forArchiveFile = DEFAULT_IN_FILE;
+            archivedFile = DEFAULT_OUT_FILE;
+            toArchive = true;
+        } else if (args[0].equals(FLAG_ARCHIVE)) {
+            forArchiveFile = args[1];
+            archivedFile = args[2];
+            toArchive = true;
+        } else if (args[0].equals(FLAG_UNARCHIVE)) {
+            archivedFile = args[1];
+            uarFile = args[2];
+        } else if (!extensionOf(args[0]).equals(ARCHIVE_EXTENSION)) {
+            forArchiveFile = args[0];
+            if (args.length > 1) {
+                archivedFile = args[1];
+            } else {
+                archivedFile = args[0] + ARCHIVE_EXTENSION;
+            }
+            toArchive = true;
+        } else if (extensionOf(args[0]).equals(ARCHIVE_EXTENSION)) {
+            archivedFile = args[0];
+            if (args.length > 1) {
+                uarFile = args[1];
+            } else {
+                uarFile = args[0].replaceAll(ARCHIVE_EXTENSION, UNARCHIVE_EXTENSION);
+            }
+            toArchive = false;
+        }
+
         try {
-            System.out.println("Start archive");
-            FileInputStream fin = new FileInputStream(IN_FILE);
+            Compressor compressor = new Compressor();
+            String inFile = toArchive ? pathToFile + forArchiveFile : pathToFile + archivedFile;
+            String outFile = toArchive ? pathToFile + archivedFile : pathToFile + uarFile;
 
+            FileInputStream fin = new FileInputStream(inFile);
             long startArchiveTime = System.nanoTime();
-
-            byte [] result = compressor.archive(fin.readAllBytes());
+            byte[] result = toArchive ? compressor.archive(fin.readAllBytes()) : compressor.unarchive(fin.readAllBytes());
             long elapsedArchiveTime = System.nanoTime() - startArchiveTime;
             double elapsedTimeInSecond = (double) elapsedArchiveTime / 1_000_000_000;
 
-            System.out.println("archive elapsed time " + elapsedTimeInSecond + " s");
-
-            FileOutputStream fot = new FileOutputStream(OUT_FILE);
+            FileOutputStream fot = new FileOutputStream(outFile);
             fot.write(result);
 
+            long inFileSize = new File(inFile).length();
+            long outFileSize = new File(outFile).length();
 
-            FileInputStream fin_a = new FileInputStream(OUT_FILE);
-            byte[] result_a = compressor.unarchive(fin_a.readAllBytes());
-            FileOutputStream fot_a = new FileOutputStream("src/com/shpp/p2p/cs/lzhukova/assignment14/assets/result.txt");
-            fot_a.write(result_a);
+            System.out.println();
+            System.out.println("input file: " + inFile);
+            System.out.println("output file: " + outFile);
+            System.out.println("input file size: " + inFileSize + " bytes");
+            System.out.println("output file size: " + outFileSize + " bytes");
+            System.out.println("time taken: " + elapsedTimeInSecond + " s");
+            System.out.println(toArchive ? "coefficient of efficiency : " +
+                    (double) outFileSize / inFileSize * 100 + " %" : "");
 
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
+
+    /**
+     * Method, that returns extension of the file;
+     *
+     * @param filename name of the file, that is going to be added to the window;
+     * @return String, extension of the file;
+     */
+    private static String extensionOf(String filename) {
+        int lastDot = filename.lastIndexOf(46);
+        return lastDot == -1 ? "" : filename.substring(lastDot);
+
+    }
+
 }
--- src/com/shpp/p2p/cs/lzhukova/assignment14/Compressor.java
+++ src/com/shpp/p2p/cs/lzhukova/assignment14/Compressor.java
@@ -1,37 +1,31 @@
 package com.shpp.p2p.cs.lzhukova.assignment14;
 
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.BitSet;
 import java.util.HashMap;
 
 public class Compressor {
 
     public byte[] archive(byte[] inBytes) {
-        System.out.println("In-array of bytes: " + Arrays.toString(inBytes));
+//        System.out.println("In-array of bytes: " + Arrays.toString(inBytes));
 
         HashMap<Byte, BitSet> codes = new HashMap<>();
         for (byte inByte : inBytes) {
             codes.put(inByte, null);
         }
 
-        byte neededBitSetSize =  (byte) (Math.log(codes.size()) / Math.log(2));
-//        int codesInBytes = (int) Math.ceil(codes.size() * neededBitSetSize / 8);
+        byte neededBitSetSize =  (byte) (Math.ceil(Math.log(codes.size()) / Math.log(2)));
         int dataInBytes = (inBytes.length * neededBitSetSize + codes.size() * neededBitSetSize) / 8 + 1;
-        System.out.println(dataInBytes);
 
         ByteBuffer buffer = ByteBuffer.allocate(4 + 8 + 1 + codes.size()  + dataInBytes);
-        System.out.println("Unique symbols " + codes.size());
-        System.out.println("Inbyte size " + inBytes.length);
-        System.out.println("neededBitSetSize " + neededBitSetSize);
+
         long fileSizeInBytes = inBytes.length;
 
         buffer.putInt(codes.size());
         buffer.putLong(inBytes.length);
         buffer.put(neededBitSetSize);
 
-        System.out.println("buffer after putting unique symbols and size of the file in bytes: ");
-//        System.out.println(Arrays.toString(buffer.array()));
+//        System.out.println("buffer after putting unique symbols and size of the file in bytes: ");
 
         BitSet bits = BitSet.valueOf(ByteBuffer.allocate(dataInBytes));
 
@@ -59,13 +53,12 @@ public class Compressor {
                 bits.set(bitIndex, codeBitSet.get(k));
             }
         }
-
-        System.out.println();
-        System.out.println("codes:" + codes.size());
-        System.out.println("bits:" + bits.size());
+//
+//        System.out.println();
+//        System.out.println("codes:" + codes.size());
+//        System.out.println("bits:" + bits.size());
 
         buffer.put(bits.toByteArray());
-//        System.out.println(Arrays.toString(buffer.array()));
         return buffer.array();
     }
 
@@ -111,6 +104,7 @@ public class Compressor {
 //        System.out.println(dataInBits.size());
         int valuesInBytes = tableSize * bitSetSize; // 40
         ByteBuffer result = ByteBuffer.allocate((int) dataSize);
+
 
         for (int i = 0, k = 0; i <= dataInBits.length(); i += bitSetSize, k++) {
             BitSet set = dataInBits.get(i, i + bitSetSize);
